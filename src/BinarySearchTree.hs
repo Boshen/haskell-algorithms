e3cd4ea576adee13d9f36709a4c6656c543ba62b
Boshen <boshenc@gmail.com>
2018-08-19 21:25:57 +0800

    add BinarySearchTree

--- src/BinarySearchTree.hs
+++ src/BinarySearchTree.hs
@@ -1,3 +1,71 @@
 module BinarySearchTree where
 
+import qualified Data.List as List
+import           Prelude   hiding (length, lookup, maximum, minimum)
 
+data Tree a
+  = Empty
+  | Node (Tree a)
+         a
+         (Tree a)
+  deriving (Show, Eq)
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert k Empty = Node Empty k Empty
+insert k (Node l x r)
+  | k < x = Node (insert k l) x r
+  | otherwise = Node l x (insert k r)
+
+fromList :: Ord a => [a] -> Tree a
+fromList = List.foldl' (flip insert) Empty
+
+toList :: Tree a -> [a]
+toList Empty        = []
+toList (Node l x r) = toList l ++ [x] ++ toList r
+
+preorder :: Tree a -> [a]
+preorder (Node l x r) = [x] ++ preorder l ++ preorder r
+preorder Empty        = []
+
+inorder :: Tree a -> [a]
+inorder = toList
+
+postorder :: Tree a -> [a]
+postorder (Node l x r) = postorder l ++ postorder r ++ [x]
+postorder Empty        = []
+
+lookup :: Ord a => a -> Tree a -> Tree a
+lookup _ Empty = Empty
+lookup k n@(Node l x r)
+  | k == x = n
+  | k < x = lookup k l
+  | otherwise = lookup k r
+
+minimum :: Tree a -> a
+minimum Empty            = error "empty tree"
+minimum (Node Empty x _) = x
+minimum (Node l x _)     = minimum l
+
+maximum :: Tree a -> a
+maximum Empty            = error "empty tree"
+maximum (Node _ x Empty) = x
+maximum (Node _ x r)     = maximum r
+
+delete :: Ord a => a -> Tree a -> Tree a
+delete _ Empty = Empty
+delete x (Node l k r)
+  | x < k = Node (delete x l) k r
+  | x > k = Node l k (delete x r)
+  | isEmpty l = r
+  | isEmpty r = l
+  | otherwise =
+    let k' = minimum r
+     in Node l k' (delete k' r)
+
+isEmpty :: Tree a -> Bool
+isEmpty Empty = True
+isEmpty _     = False
+
+length :: Tree a -> Int
+length Empty        = 0
+length (Node l x r) = length l + 1 + length r
--- test/Test/BinarySearchTree.hs
+++ test/Test/BinarySearchTree.hs
@@ -1,12 +1,59 @@
 module Test.BinarySearchTree where
 
 import Test.Hspec
+import Test.QuickCheck
 
-import BinarySearchTree
+import BinarySearchTree (Tree(..))
+import qualified BinarySearchTree as T
 
 testBinarySearchTree :: Spec
-testBinarySearchTree = do
-  describe "test" $ do
-    it "test" $ do
-      True `shouldBe` True
+testBinarySearchTree = describe "binary search tree" $ do
+  describe "insert" $ do
+    specify "empty" $ property $
+      \x -> T.insert (x :: Int) Empty == Node Empty x Empty
 
+    specify "left" $ property $ \x ->
+      let node = T.insert 1 Empty
+      in x < 1 ==> T.insert (x :: Int) node == Node (Node Empty x Empty) 1 Empty
+
+    specify "right" $ property $ \x ->
+      let node = T.insert 1 Empty
+      in x >= 1 ==> T.insert (x :: Int) node == Node Empty 1 (Node Empty x Empty)
+
+  specify "fromList" $
+    T.fromList [1, 2] `shouldBe` Node Empty 1 (Node Empty 2 Empty)
+
+  specify "toList" $
+    T.toList (Node (Node Empty 1 Empty) 2 Empty) `shouldBe` [1, 2]
+
+  specify "preorder" $
+    T.preorder tree `shouldBe` [4, 3, 1, 2, 8, 7, 16, 10, 9, 14]
+
+  specify "inorder" $
+    T.inorder tree `shouldBe` [1, 2, 3, 4, 7, 8, 9, 10, 14, 16]
+
+  specify "postorder" $
+    T.postorder tree `shouldBe` [2, 1, 3, 7, 9, 14, 10, 16, 8, 4]
+
+  specify "lookup" $ do
+    T.lookup 0 Empty `shouldBe` Empty
+    T.lookup 14 tree `shouldBe` Node Empty 14 Empty
+    T.lookup 9 tree `shouldBe` Node Empty 9 Empty
+
+  specify "maximum" $ property $ \list ->
+    length list /= 0 ==> T.maximum (T.fromList list) == maximum (list :: [Int])
+
+  specify "minimum" $ property $ \list ->
+    length list /= 0 ==> T.minimum (T.fromList list) == minimum (list :: [Int])
+
+  specify "delete" $ do
+    T.delete 0 Empty `shouldBe` Empty
+    T.delete 3 (T.fromList [2, 1, 3]) `shouldBe` Node (Node Empty 1 Empty) 2 Empty
+    T.delete 1 (T.fromList [2, 1, 3]) `shouldBe` Node Empty 2 (Node Empty 3 Empty)
+    T.delete 2 (T.fromList [2, 1, 3]) `shouldBe` Node (Node Empty 1 Empty) 3 Empty
+
+  specify "length" $ property $ \list ->
+    T.length (T.fromList list) == length (list :: [Int])
+
+tree :: Tree Int
+tree = T.fromList [4, 3, 1, 2, 8, 7, 16, 10, 9, 14]
